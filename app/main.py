e919bebdc53d77bcb8362f6a25733db6b84c4e26
Serhii Karavashkin <serhii.karavashkin@gmail.com>
2025-08-16 18:54:56 +0300

    refactor(main.py): change calling controller

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI, UploadFile, Query, File, HTTPException, BackgroundTasks
+from fastapi import (
+    FastAPI,
+    UploadFile,
+    Query,
+    File,
+    HTTPException,
+    BackgroundTasks,
+    Depends,
+)
 from fastapi.responses import JSONResponse
+from functools import lru_cache
 from typing import Annotated
 from pathlib import Path
 import tempfile
@@ -10,25 +19,38 @@ from app.schemas.book import Answer, MetaBook
 from app.core import config as settings
 
 app = FastAPI(title="Bookshelf API")
-controller = BookshelfService()
 
 
-@app.get("/bookshelf", summary="Get Indexed Books", response_model=list[str])
-async def get_books_list():
-    return await controller.get_all_books()
+@lru_cache()
+def get_bookshelf_singleton() -> BookshelfService:
+    return BookshelfService()
 
 
-@app.get("/bookshelf/{book_id}", summary="Get Meta from Book", response_model=MetaBook)
-async def get_book_info(book_id: str):
-    book = await controller.get_book(book_id)
+@app.get("/bookshelf", summary="Get Indexed Books")
+async def get_books_list(
+    service: BookshelfService = Depends(get_bookshelf_singleton),
+) -> list[str]:
+    return await service.get_all_books()
+
+
+@app.get("/bookshelf/{book_id}", summary="Get Meta from Book")
+async def get_book_info(
+    book_id: str,
+    service: BookshelfService = Depends(get_bookshelf_singleton),
+) -> MetaBook:
+    book = await service.get_book(book_id)
 
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
 
-@app.post("/bookshelf", summary="Add Book", response_model=str)
-async def upload_book(background: BackgroundTasks, file: UploadFile = File(...)):
+@app.post("/bookshelf", summary="Add Book")
+async def upload_book(
+    background: BackgroundTasks,
+    file: UploadFile = File(...),
+    service: BookshelfService = Depends(get_bookshelf_singleton),
+) -> str:
     # basic validation
     if not file:
         raise HTTPException(status_code=400, detail="File is required")
@@ -45,11 +67,11 @@ async def upload_book(background: BackgroundTasks, file: UploadFile = File(...))
     with open(tmp_path, "wb") as f:
         f.write(contents)
 
-    # enqueue in controller: returns book_id
-    book_id = controller.enqueue_file(tmp_path, filename)
+    # enqueue in service: returns book_id
+    book_id = service.enqueue_file(tmp_path, filename)
 
     # schedule background processing
-    background.add_task(controller.process_file, book_id)
+    background.add_task(service.process_file, book_id)
 
     return JSONResponse(
         status_code=202, content={"doc_id": book_id, "status": "accepted"}
@@ -57,21 +79,30 @@ async def upload_book(background: BackgroundTasks, file: UploadFile = File(...))
 
 
 @app.get("/bookshelf/{book_id}/status")
-def get_status(book_id: str):
-    status = controller.get_job_status(book_id)
+def get_status(
+    book_id: str,
+    service: BookshelfService = Depends(get_bookshelf_singleton),
+):
+    status = service.get_job_status(book_id)
     if not status:
         raise HTTPException(status_code=404, detail="book_id not found")
     return status
 
 
-@app.post(
-    "/bookshelf/{book_id}", summary="Send Question from Book", response_model=Answer
-)
-async def ask_question(book_id: str, q: Annotated[str, Query(max_length=300)]):
-    return await controller.ask_book_question(book_id, q)
+@app.post("/bookshelf/{book_id}", summary="Send Question from Book")
+async def ask_question(
+    book_id: str,
+    q: Annotated[str, Query(max_length=300)],
+    service: BookshelfService = Depends(get_bookshelf_singleton),
+) -> Answer:
+    return await service.ask_book_question(book_id, q)
 
 
 @app.delete("/bookshelf/{book_id}", summary="Remove Book")
-async def delete_book(book_id: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(controller.remove_book, book_id)
+async def delete_book(
+    book_id: str,
+    background_tasks: BackgroundTasks,
+    service: BookshelfService = Depends(get_bookshelf_singleton),
+):
+    background_tasks.add_task(service.remove_book, book_id)
     return {"status": "accepted", "book_id": book_id}
