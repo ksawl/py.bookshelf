6bd14e0af37c5a7f0d3c1cdd935e72e684195c4b
Serhii Karavashkin <serhii.karavashkin@gmail.com>
2025-08-16 11:29:10 +0300

    add .env.template & change config

--- app/core/config.py
+++ app/core/config.py
@@ -4,34 +4,25 @@ from dotenv import load_dotenv
 if not os.getenv("PINECONE_API_KEY"):
     load_dotenv()
 
-PINECONE_FREE_TIER = True
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_ENV = os.getenv("PINECONE_ENV", "us-east-1")  # e.g., "us-west1-gcp"
-PINECONE_INDEX = os.getenv("PINECONE_INDEX", "bookshelf-index")
-PINECONE_SERVERLESS_CLOUD = os.getenv(
-    "PINECONE_SERVERLESS_CLOUD", "aws"
-)  # e.g. ['gcp', 'aws', 'azure']
-PINECONE_SERVERLESS_REGION = os.getenv("PINECONE_SERVERLESS_REGION", "us-east-1")
-
-CHUNK_SIZE = int(os.getenv("CHUNK_TOKENS", "500"))
-CHUNK_TOKENS = int(os.getenv("CHUNK_TOKENS", "500"))
-OVERLAP_PCT = float(os.getenv("OVERLAP_PCT", "0.2"))
-BATCH_SIZE = int(os.getenv("BATCH_SIZE", "100"))
-ENCODING_NAME = "cl100k_base"  # for tiktoken
-
-# choose model: "all-MiniLM-L6-v2" (fast, 384 dim) or "all-mpnet-base-v2" (768 dim)
-EMBED_MODEL_NAME = "all-MiniLM-L6-v2"
-
-
-# https://platform.openai.com/api-keys
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # required for embeddings in this example
-EMBEDDING_MODEL = os.getenv(
-    "EMBEDDING_MODEL", "text-embedding-3-small"
-)  # change if needed
-
 ALLOWED_EXTENSIONS = {"docx", "odt", "pdf", "txt"}
 
-OLLAMA_API_BASE_URL = "http://localhost:11434"
-LLM_MODEL = "llama2"
-MAX_CONTEXT_CHARS = 4000
-TOP_K = 5
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENV = os.getenv("PINECONE_ENV")
+PINECONE_INDEX = os.getenv("PINECONE_INDEX")
+PINECONE_SERVERLESS_CLOUD = os.getenv("PINECONE_SERVERLESS_CLOUD")
+PINECONE_SERVERLESS_REGION = os.getenv("PINECONE_SERVERLESS_REGION")
+
+CHUNK_TOKENS = int(os.getenv("CHUNK_TOKENS"))
+OVERLAP_PCT = float(os.getenv("OVERLAP_PCT"))
+BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
+ENCODING_NAME = os.getenv("ENCODING_NAME")
+
+EMBED_MODEL_NAME = os.getenv("EMBED_MODEL_NAME")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
+
+OLLAMA_API_BASE_URL = os.getenv("OLLAMA_API_BASE_URL")
+LLM_MODEL = os.getenv("LLM_MODEL")
+MAX_CONTEXT_CHARS = int(os.getenv("MAX_CONTEXT_CHARS"))
+TOP_K = int(os.getenv("TOP_K"))
